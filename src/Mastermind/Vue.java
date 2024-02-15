ff774a042240d44ca5db55af8434b2fc869f599f
had-esf <adrien.velleret@gmail.com>
2024-02-15 09:37:28 +0100

    base de la frame

--- src/Mastermind/Vue.java
+++ src/Mastermind/Vue.java
@@ -1,8 +1,25 @@
 package Mastermind;
 
-public class Vue {
-	public Vue() {
-		
-	}
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Frame;
+import java.awt.Panel;
 
+public class Vue{
+	public Vue() {
+		Frame f = new Frame();
+		Panel VueClavier = new Panel();
+		Canvas VueProposition = new Canvas();
+		
+		f.add(VueClavier);
+		f.add(VueProposition);
+		
+		
+		f.setSize(500, 500);
+		f.setTitle("Mastermind");
+		f.setLayout(new BorderLayout());
+		f.setVisible(true);
+	}
+	
+	
 }
