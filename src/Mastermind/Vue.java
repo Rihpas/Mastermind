dedb7c4ab1a6dd0ef0658547c1d55195125e338b
had-esf <adrien.velleret@gmail.com>
2024-02-15 09:59:22 +0100

    ajout boutons colorés

--- src/Mastermind/Vue.java
+++ src/Mastermind/Vue.java
@@ -1,25 +1,38 @@
 package Mastermind;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Canvas;
 import java.awt.Frame;
 import java.awt.Panel;
 
-public class Vue{
+public class Vue extends Frame{
+	Modele modl;
 	public Vue() {
-		Frame f = new Frame();
+		this.modl = new Modele();
+		
+		this.setLayout(new BorderLayout());
+
 		Panel VueClavier = new Panel();
 		Canvas VueProposition = new Canvas();
 		
-		f.add(VueClavier);
-		f.add(VueProposition);
+		for(int i=0; i<modl.couleur.length; i++) {
+			Button b = new Button();
+			b.setBackground(this.modl.couleur[i]);
+			VueClavier.add(b);
+		}
 		
+		this.add(VueClavier, BorderLayout.SOUTH);
+		this.add(VueProposition, BorderLayout.NORTH);
 		
-		f.setSize(500, 500);
-		f.setTitle("Mastermind");
-		f.setLayout(new BorderLayout());
-		f.setVisible(true);
+		this.setSize(500, 500);
+		this.setTitle("Mastermind");
+		
+		this.setVisible(true);
 	}
 	
+	public static void main(String[] args) {
+		Vue v = new Vue();
+	}
 	
 }
