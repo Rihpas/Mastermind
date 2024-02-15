8fd8ab2e63ba94601d54c4bbe4c0b17e2f926f86
Rihpas <lea.cauquil0@gmail.com>
2024-02-15 09:19:32 +0100

    les attributen public

--- src/Mastermind/Modele.java
+++ src/Mastermind/Modele.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class Modele {
-	ArrayList<Color> couleur=new ArrayList<>();
-	int n_tentatives ;
-	int difficulte;
-	enum etat {EN_COURS,GAGNE,PERDU};
+	public ArrayList<Color> couleur=new ArrayList<>();
+	public int n_tentatives ;
+	public int difficulte;
+	public enum etat {EN_COURS,GAGNE,PERDU};
 }
