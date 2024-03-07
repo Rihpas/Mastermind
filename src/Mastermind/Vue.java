6f9b0658214419487126708f2e6203c799da63ac
had-esf <adrien.velleret@gmail.com>
2024-03-07 09:34:25 +0100

    emplacement pions et réponses

--- src/Mastermind/Vue.java
+++ src/Mastermind/Vue.java
@@ -32,14 +32,29 @@ public class Vue extends Frame{
 		this.add(VueClavier, BorderLayout.SOUTH);
 		this.add(VueProposition, BorderLayout.NORTH);
 		
-		this.setSize(500, 500);
+		this.setSize(400, 750);
 		this.setTitle("Mastermind");
 		
 		this.setVisible(true);
 	}
 	
 	public void paint(Graphics g) {
-		g.drawOval(0, 100, 100, 100);
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<10; j++) {
+				g.drawOval(5+i*90, 40+j*60, 50, 50);
+			}
+		}
+		
+		for(int x=0; x<10; x++) {
+			for(int i=0; i<2; i++) {
+				for(int j=0; j<2; j++) {
+					g.drawOval(350+i*20, 45+j*20+x*60, 15, 15);
+				}
+			}
+		}
+		
+		
+		
 	}
 	
 	public static void main(String[] args) {
