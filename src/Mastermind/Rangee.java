597c17ed76acefde9d8bb0bfe6063627ce3a1e81
Rihpas <lea.cauquil0@gmail.com>
2024-03-07 09:00:06 +0100

    resultat

--- src/Mastermind/Rangee.java
+++ src/Mastermind/Rangee.java
@@ -2,8 +2,10 @@ package Mastermind;
 
 import java.awt.Color;
 
+
 public class Rangee {
 	public Color[] jetons=new Color[Modele.Difficulte];
 	public int indiceJeton=0;
+	public Integer[] resultat=new Integer[2] ;
 	
 }
