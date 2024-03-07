7fe08fd044eb1998a0fcff0e85cfd4f9808fd61f
had-esf <adrien.velleret@gmail.com>
2024-03-07 09:02:41 +0100

    tentative de windowListener

---
+++ src/Mastermind/Controleur.java
@@ -0,0 +1,58 @@
+package Mastermind;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Controleur implements WindowListener{
+	Vue vue;
+	public Controleur() {
+		vue.addWindowListener(this);
+	}
+	
+	public static void main(String[] args) {
+		Controleur c = new Controleur();
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
