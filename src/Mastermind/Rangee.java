84cfb42c3e5f4d1bd544440c75cb647b81fa40fd
Rihpas <lea.cauquil0@gmail.com>
2024-03-07 09:42:23 +0100

    bon2

--- src/Mastermind/Rangee.java
+++ src/Mastermind/Rangee.java
@@ -4,10 +4,15 @@ import java.awt.Color;
 
 
 public class Rangee {
-	public Color[] jetons=new Color[Modele.Difficulte];
+	public Color[] jetons;
 	public int indiceJeton=0;
 	public int nbNoir; //bonne couleur bien placer 
 	public int nbBlanc; // bonne couleur mal placer
 	public Integer[] resultat= {nbNoir,nbBlanc} ;
+	
+	public Rangee () {
+		jetons=new Color[Modele.Difficulte];
+	}
+	
 	
 }
